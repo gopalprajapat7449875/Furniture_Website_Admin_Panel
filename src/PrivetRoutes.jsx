5d05abbd142a51c98f3586f39da5f50e5b22dcee
gopalprajapat7449875 <gopalprajapat7449875@gmail.com>
2026-05-30 13:41:48 +0530

    elen

--- src/PrivetRoutes.jsx
+++ src/PrivetRoutes.jsx
@@ -1,7 +1,7 @@
 import { Navigate } from "react-router-dom";
 import Cookies from 'js-cookie'
 const PrivateRoute = ({ children }) => {
-  const token = Cookies.get("atoken");
+  const token = Cookies.get("token");
 
   if (!token) {
     return <Navigate to="/" />; // login page
